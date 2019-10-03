a6c71d14c954a9394a226d71d9e8d7f5ff314b2d
MirajiPrasadini <39193782+MirajiPrasadini@users.noreply.github.com>
2019-10-03 12:00:42 +0530

    Create Hacktomberfest_Helloworld.java

---
+++ scripts/HelloHacktomberfeast2k19.java
@@ -0,0 +1,10 @@
+public class{
+  public Static void main(String args[]){
+  
+  System.out.println("Hello I'm Miraji.");
+  System.out.println("Happy Coding :) ");
+  for(i=1;i<6;i++){
+  System.out.println("Hello World..!");
+    }
+  }
+}
